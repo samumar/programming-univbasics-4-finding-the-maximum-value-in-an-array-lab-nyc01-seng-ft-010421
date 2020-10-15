1057f9ecae46862d04c1b4cc4a5e21b4a547548f
samumar43 <samumar43@gmail.com>
2020-10-15 16:41:56 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,5 +1,9 @@
 def find_max_value(array)
   count = 0 
+  value = nil 
   while count < array.length do 
-    
+    if array.index == value > array.size(array[count])
+      value
+    end 
+    count += 1
 end
